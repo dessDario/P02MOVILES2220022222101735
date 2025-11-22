8318d3eb07c62af2db7e268306f3a1112714afb9
Willman Alca Alfaro <22101735@ue.edu.pe>
2025-11-22 09:30:47 -0500

    second commit

---
+++ app/src/main/java/com/example/p02moviles2220022222101735/data/remote/firebase/FirebaseAuthManager.kt
@@ -0,0 +1,4 @@
+package com.example.p02moviles2220022222101735.data.remote.firebase
+
+object FirebaseAuthManager {
+}
---
+++ app/src/main/java/com/example/p02moviles2220022222101735/presentation/navigation/DrawerScaffold.kt
@@ -0,0 +1,2 @@
+package com.example.p02moviles2220022222101735.presentation.navigation
+
